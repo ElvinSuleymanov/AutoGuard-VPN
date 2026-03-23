cd54cc2037cf37e68e425a1e9abbf78c531727bc
ElvinSuleymanov <elvinsuleymanov150@gmail.com>
2026-03-24 00:42:39 +0400

    proper health check on auth service

--- auth/main.py
+++ auth/main.py
@@ -4,6 +4,15 @@ from fastapi import FastAPI, Security
 from helpers import verify
 app = FastAPI()
 
+ 
+@app.get("/health")
+def health_check():
+    try:
+        os.subprocess.run(["wg show wg0"], check=True, capture_output=True)
+        return {"status":"ok"}
+    except:
+        return {"status":"wg0 is not ready"}
+
 @app.get("/", dependencies=[Security(verify)])
 def authenticate_client_script():
     requests.post()
