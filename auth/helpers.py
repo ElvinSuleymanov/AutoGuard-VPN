ef61443262fb46455220e3ba26423a65a34ee187
ElvinSuleymanov <elvinsuleymanov150@gmail.com>
2026-03-24 00:34:08 +0400

    wrong token

--- auth/helpers.py
+++ auth/helpers.py
@@ -3,10 +3,10 @@ import secrets
 from fastapi import HTTPException, Security
 from fastapi.security import APIKeyHeader
 
-AUTH_KEY_SCRIPT = os.environ["AUTH_KEY_SCRIPT"]
+REGISTRATION_TOKEN = os.environ["REGISTRATION_TOKEN"]
 api_key_header = APIKeyHeader(name="X-Auth-Token")
 
 
 def verify(key: str = Security(api_key_header)):
-    if not secrets.compare_digest(key, AUTH_KEY_SCRIPT):
+    if not secrets.compare_digest(key, REGISTRATION_TOKEN):
         raise HTTPException(status_code=401, detail="Invalid token")
